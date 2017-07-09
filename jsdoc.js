ca0c8fbbb8aaaa38f1d7ab6b59d1cb9ccdf4e7f7
Allen3039 <623052720@qq.com>
2017-07-09 19:11:52 +0800

    add jsdoc

---
+++ jsdoc.js
@@ -0,0 +1,25 @@
+/**
+ * 登陆接口
+ * @param  {string} username 
+ * @param  {} pwd 
+ */
+function login(username, pwd) {
+
+}
+var name = 'allen';
+var pwd = '123';
+login('allen3039', '5613331');
+if (name === 'wk') {
+    console.log('your name is same as mine!');
+}
+
+/**
+ * 啥东西哦     
+ * 
+ * @param {Number} a 
+ * @param {Number} b 
+ * @returns {Int} 
+ */
+function Add(a : Number, b : Number) : Int {
+    return a + b;
+}
